17163a154a7fe75f8d03a008c89a2c5d2018a078
awatefkh3 <awatefkh3@gmail.com>
2021-10-20 14:25:49 +0300

    added things

--- app/src/main/java/com/example/myfirsrtpro/CalendarAdapter.java
+++ app/src/main/java/com/example/myfirsrtpro/CalendarAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myfirsrtpro;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -10,25 +11,34 @@ import java.util.ArrayList;
 
 public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>{
     private final ArrayList<String> daysOfMonth;
+    private final OnItemListener onItemListener ;
 
     public CalendarAdapter(ArrayList<String> daysOfMonth) {
         this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
     }
 
     @NonNull
     @Override
     public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        return null;
+        View view=inflater.inflate(R.layout.calendar_cell, parent, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        return new CalendarViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
-
+        holder.dayOfMonth.setText(daysOfMonth.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return daysOfMonth.size();
+    }
+
+    public interface OnItemListener{
+        void onItemClick(int position,String dayText);
     }
 }
